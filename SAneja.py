e69eb1fc1d49c9967eba71b91cac461d3cabec48
saurabhaneja <saurabh.aneja@duke.edu>
2015-01-28 15:08:50 -0500

    Assignmetn

--- SAneja.py
+++ SAneja.py
@@ -1,18 +1,12 @@
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
-import csv
 
 
-main_dir = "/Users/saurabh/Desktop/Github"
+main_dir = "/Users/saurabh/Desktop/Github/PubPol590"
 txt_file = "/Data/File1_small.txt"
 
-saurabh = read_table(File1_small.txt)
+saurabh = pd.read_table(main_dir + txt_file, sep(" ")
 
-f = open(File1_small.txt, '')
-try:
-    reader = csv.DictReader(f)
-    for row in reader:
-        print row
-finally:
-    f.close()
+saurabh[60:100]
+saurabh[saurabh.kwh >= 30]
